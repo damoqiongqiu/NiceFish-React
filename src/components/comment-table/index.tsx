093d87e876b8772e7084ff491560c8cdf729c0e2
燕云长风 <hbxyxuxiaodong@163.com>
2019-03-29 13:09:32 +0800

    finish post-table comment-table profile and rebuild

--- src/components/comment-table/index.tsx
+++ src/components/comment-table/index.tsx
@@ -1,6 +1,63 @@
 import * as React from 'react';
-
+import {useState} from 'react';
+import {Table} from 'antd';
 function CommentTable(){
-    return (<div></div>)
+    const columns = [
+        {
+          title: "序号",
+          dataIndex: "key"
+        },
+        {
+          title: "内容",
+          dataIndex: "content"
+        },
+        {
+          title: "用户",
+          dataIndex: "userName"
+        },
+        {
+          title: "日期",
+          dataIndex: "time"
+        }
+      ];
+    
+     const [data]= useState([
+        { key: '1', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '2', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '3', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '4', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '5', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '6', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '7', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '8', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '9', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '10', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '11', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '12', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '13', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' },
+        { key: '14', content: '这是一条不合法的评论', userName: 'damoqiongqiu', time: '2017-07-15 16:22:58' }
+      ])
+    return (<div className="comment-table-container" >
+    <form className="form-vertical" role="form">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="input-group">
+                    <input className="form-control" type="text" placeholder="内容，用户名"/>
+                    <span className="input-group-btn">
+                        <button className="btn btn-default" type="button"><i className="fa fa-search" ></i>
+                            </button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div className="row mt-16px">
+        <div className="col-md-12">
+            <div className="comment-item-container">
+               <Table dataSource={data} columns={columns}></Table>
+            </div>
+        </div>
+    </div>
+</div>)
 }
 export default CommentTable
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,115 @@
-import * as React from 'react';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Transfer } from "antd";
+import { string } from "prop-types";
+function Profile() {
+  const formControls = [
+    { type: "image", label: "", key: "", value: "", placeholder: "", src: "" },
+    { type: "input", label: "头像", key: "", value: "", placeholder: "" },
+    { type: "input", label: "用户名", key: "", value: "", placeholder: "" },
+    { type: "input", label: "常用邮箱", key: "", value: "", placeholder: "" },
+    { type: "input", label: "密码", key: "", value: "", placeholder: "" },
+    { type: "input", label: "确认密码", key: "", value: "", placeholder: "" },
+    { type: "textarea", label: "个人简介", key: "", value: "", placeholder: "" }
+  ];
 
-function Profile(){
-    return (<div></div>)
+  const [mockData, updateMockData] = useState([] as any);
+  const [targetKeys, updatetargetKeys] = useState([] as any);
+  function getMock() {
+    let mockData = [] as any;
+    let targetKeys = [] as any;
+    mockData = [
+      {
+        key: "1",
+        title: "默认权限",
+        chosen: false
+      },
+      {
+        key: "2",
+        title: "签约作者",
+        chosen: false
+      },
+      {
+        key: "3",
+        title: "系统管理员",
+        chosen: false
+      }
+    ];
+    updatetargetKeys([...targetKeys]);
+    updateMockData([...mockData]);
+  }
+  function handleChange(targetKeys: any) {
+    updatetargetKeys(targetKeys);
+  }
+
+  useEffect(() => {
+    getMock();
+  }, []);
+  return (
+    <div className="user-profile-container">
+      <div className="card">
+        <div className="card-header">基本资料</div>
+        <div className="pd-16px">
+          <form role="form">
+            {formControls.map((item, index) => {
+              return (
+                <div className="form-group row text-align-right" key={index}>
+                  <label className="col-md-2 control-label">{item.label}</label>
+                  <div className="col-md-10">
+                    <div>
+                      {item.type === "input" ? (
+                        <input className="form-control" />
+                      ) : (
+                        ""
+                      )}
+                      {item.type === "textarea" ? (
+                        <textarea
+                          className="form-control"
+                          placeholder={item.placeholder}
+                        />
+                      ) : (
+                        ""
+                      )}
+
+                      {item.type === "image" ? <img src={item.src} /> : ""}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </form>
+        </div>
+      </div>
+      <div className="card mt-16px">
+        <div className="card-header">关联角色</div>
+        <div className="pd-16px">
+          <form>
+            <div className="form-group">
+              <div className="col-md-12">
+                <Transfer
+                  dataSource={mockData}
+                  targetKeys={targetKeys}
+                  onChange={handleChange}
+                  render={item => item.title}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <form role="form">
+        <div className="form-group row mt-16px">
+          <div className="col-md-10">
+            <button type="button" className="btn btn-primary btn-margin-1rem">
+              保存
+            </button>
+            <button type="button" className="btn btn-default">
+              取消
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Profile
+export default Profile;
