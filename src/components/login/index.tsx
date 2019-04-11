6fb61c9ba90b1c10d7ccbe066d53b93d840160bc
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-11 15:24:34 +0800

    add login validator and rebuild

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "antd";
 import Common from "../../utils/common.util";
+import {loginFormValidator} from '../../validator/login-form-validator';
 import "./index.scss";
+
 function Login() {
   const [namefill, updateNamefill] = useState("");
   const [pwdfill, updatePwdfill] = useState("");
@@ -10,6 +12,13 @@ function Login() {
     name: "",
     pwd: ""
   });
+  const [errors,setErrors] = useState({} as any);
+  const [meta,setMeta] = useState({name:{touched:false},pwd:{touched:false}});
+  const [formValid ,setformValid] = useState(false);
+  function onFocus(key:any){
+     setMeta({...meta,[key]:{touched:true}});
+     setErrors(loginFormValidator(login))
+  }
   function onBlur(key: any, value: any) {
 
     switch (key) {
@@ -27,10 +36,16 @@ function Login() {
       ...value
     };
     updateLogin(uplogin);
+    setErrors(loginFormValidator(uplogin));
   }
   function doLogin() {
     console.log(login);
   }
+  useEffect(()=>{
+    const errors = loginFormValidator(login);
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
+    setformValid(isDisabled);
+  },[errors])
   return (
   
       <div className="container d-flex align-items-center">
@@ -42,36 +57,42 @@ function Login() {
             <div className="col-12 d-flex  text-white">
               <span className="inputfiled">
                 <input
-                  className={`col input-text ${namefill}`}
+                  className={`col input-text ${namefill} ${(meta.name.touched&&errors.name)?'error':''}`}
                   type="text"
                   autoComplete="off"
                   value={login.name}
+                  onFocus = {e=>{onFocus('name')}}
                   onChange={e => handleChange({ name: e.target.value })}
                   onBlur={e => onBlur("name", e.target.value)}
                 />
                 <label>Username</label>
+                {meta.name.touched&&errors.name?<div className="text-red">{errors.name}</div>:''}
               </span>
+              
             </div>
             <div className="col-12 d-flex justify-content-center  text-white">
               <span className="inputfiled">
                 <input
-                  className={`col input-text ${pwdfill}`}
+                  className={`col input-text ${pwdfill} ${meta.pwd.touched&&errors.pwd ? 'error':''}`}
                   type="password"
                   value={login.pwd}
                   autoComplete="off"
+                  onFocus = {e=>{onFocus('pwd')}}
                   onBlur={e => {
                     onBlur("pwd", e.target.value);
                   }}
                   onChange={e => handleChange({ pwd: e.target.value })}
                 />
                 <label>Password</label>
+                {meta.pwd.touched&&errors.pwd?<div className="text-red">{errors.pwd}</div>:''}
               </span>
             </div>
             <div className="col-12 d-flex justify-content-center  text-white ui-fluid">
               <Button
                 type="primary"
                 className="col bg-primary border-color-primary"
-                onClick={() => doLogin()}
+                disabled={formValid}
+                onClick={()=> doLogin()}
               >
                 登录
               </Button>
---
+++ src/validator/login-form-validator.ts
@@ -0,0 +1,14 @@
+export function loginFormValidator(values:any){
+    let errors:any = {};
+    if (!values.name) {
+      errors.name = '用户名为必填';
+    } else if (!/\w{5,}/.test(values.name)) {
+      errors.name = '用户名最少为5位';
+    }
+    if (!values.pwd) {
+      errors.pwd = '密码为必填项';
+    } else if (!/\w{6,}/.test(values.pwd)) {
+      errors.pwd = '密码最少为6位';
+    }
+    return errors;
+}
