9d1107ad95d2efbd836e84d928ff095b7ef0b2ad
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-12 10:20:43 +0800

    add register validator and rebuild

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "antd";
+import { registerFormValidator} from '../../validator/register-form-validator';
+import { withRouter } from 'react-router-dom';
+import storageService from '../../service/storage.service';
 import Common from "../../utils/common.util";
-function Register() {
+function Register(props:any) {
   const [emailfill, updateEmailfill] = useState("");
   const [pwdfill, updatePwdfill] = useState("");
   const [cfpwdfill, updateCfPwdfill] = useState("");
+  const [formValid,setFormValid] = useState(false);
+  const [meta,setMeta] = useState({email:{touched:false},pwd:{touched:false},cfpwd:{touched:false}});
+  const [errors,setErrors] = useState({} as any);
   const [register, updateRegister] = useState({
     email: "",
     pwd: "",
@@ -15,12 +21,18 @@ function Register() {
   function onBlur(key: any, value: any) {
     switch (key) {
       case "email":
+        setErrors(registerFormValidator(register));
+        setMeta({...meta,[key]:{touched:true}});
         Common.toggleClass(value, updateEmailfill, Common.fillClass);
         break;
       case "pwd":
+        setErrors(registerFormValidator(register));
+        setMeta({...meta,[key]:{touched:true}});
         Common.toggleClass(value, updatePwdfill, Common.fillClass);
         break;
       case "cfpwd":
+        setErrors(registerFormValidator(register));
+        setMeta({...meta,[key]:{touched:true}});
         Common.toggleClass(value, updateCfPwdfill, Common.fillClass);
         break;
     }
@@ -31,10 +43,18 @@ function Register() {
       ...value
     };
     updateRegister(upRegister);
+    setErrors(registerFormValidator(upRegister));
   }
-  function doRegister() {
-    console.log(register);
+  function doRegister(e:any) {
+    e.preventDefault();
+    storageService.setKeyValue('user',register.email);
+    props.history.push('/home');
   }
+  useEffect(() => {
+      const errors = registerFormValidator(register);
+      const isDisabled = Object.keys(errors).some(x=> errors[x]);
+      setFormValid(isDisabled)
+  },[errors])
   return (
     
       <div className="container d-flex align-items-center">
@@ -42,11 +62,11 @@ function Register() {
           <div className="col-12 d-flex justify-content-center  text-white">
             <span className="bg-red pd-5-10px font-size-24">魚</span>
           </div>
-          <form>
+          <form onSubmit={e=>doRegister(e)}>
             <div className="col-12 d-flex  text-white">
               <span className="inputfiled">
                 <input
-                  className={`col input-text ${emailfill}`}
+                  className={`col input-text ${emailfill} ${meta.email.touched&&errors.email?'error':''}`}
                   type="text"
                   autoComplete="off"
                   value={register.email}
@@ -54,12 +74,13 @@ function Register() {
                   onBlur={e => onBlur("email", e.target.value)}
                 />
                 <label>Email</label>
+                {meta.email.touched&&errors.email?<div className="text-red">{errors.email}</div>:''}
               </span>
             </div>
             <div className="col-12 d-flex justify-content-center  text-white">
               <span className="inputfiled">
                 <input
-                  className={`col input-text ${pwdfill}`}
+                  className={`col input-text ${pwdfill} ${meta.pwd.touched&&errors.pwd?'error':''}`}
                   type="password"
                   value={register.pwd}
                   autoComplete="off"
@@ -69,12 +90,13 @@ function Register() {
                   onChange={e => handleChange({ pwd: e.target.value })}
                 />
                 <label>Password</label>
+                {meta.pwd.touched&&errors.pwd?<div className="text-red">{errors.pwd}</div>:''}
               </span>
             </div>
             <div className="col-12 d-flex justify-content-center  text-white">
               <span className="inputfiled">
                 <input
-                  className={`col input-text ${cfpwdfill}`}
+                  className={`col input-text ${cfpwdfill} ${meta.cfpwd.touched&&errors.cfpwd?'error':''}`}
                   type="password"
                   value={register.cfpwd}
                   autoComplete="off"
@@ -84,13 +106,15 @@ function Register() {
                   onChange={e => handleChange({ cfpwd: e.target.value })}
                 />
                 <label>Confirm Password</label>
+                {meta.cfpwd.touched&&errors.cfpwd?<div className="text-red">{errors.cfpwd}</div>:''}
               </span>
             </div>
             <div className="col-12 d-flex justify-content-center  text-white ui-fluid">
               <Button
                 type="primary"
                 className="col bg-primary border-color-primary"
-                onClick={() => doRegister()}
+                htmlType="submit"
+                disabled={formValid}
               >
                 注册
               </Button>
@@ -101,4 +125,4 @@ function Register() {
 
   );
 }
-export default Register;
+export default withRouter(Register);
---
+++ src/validator/register-form-validator.ts
@@ -0,0 +1,27 @@
+
+
+
+export function registerFormValidator(values:any){
+    let errors:any = {};
+    let pwd = values.pwd;
+    let cfpwd = values.cfpwd;
+    let emailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+    if(!values.email){
+        errors.email = '邮箱必须输入';
+    }else if(!emailReg.test(values.email)){
+        errors.email = '请输入正确的邮箱格式';
+    }
+    if(!values.pwd){
+        errors.pwd = "请输入密码";
+    }else if(!/\w{8,}/.test(values.pwd)){
+        errors.pwd = '密码至少为8位';
+    }
+    if(!values.cfpwd){
+        errors.cfpwd = "请输入确认密码";
+    }else if(!/\w{8,}/.test(values.cfpwd)){
+        errors.cfpwd = '确认密码至少为8位';
+    }else if(pwd!==cfpwd){
+        errors.cfpwd = '密码不一致';
+    }
+    return errors;
+}
