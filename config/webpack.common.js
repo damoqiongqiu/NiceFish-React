7cc90070d4c853823a1d92ee2a8ea5bbb64d3b5f
yanyunchangfeng <yanyunchangfeng@gmail.com>
2022-09-09 23:19:29 +0800

    perf:应用性能优化

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,26 +8,27 @@ const smw = new SpeedMeasureWebpackPlugin(); // 费时分析的插件
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const os = require("os");
 const isDev = process.env.NODE_ENV === "development";
 isAnalyzerMode = process.env.ANALYZE === "1";
 const noop = () => {};
 // module.exports = smw.wrap({ //需要包裹一层配置对象
 module.exports = {
-  context: path.join(process.cwd()),
+  context: process.cwd(),
   entry: {
     main: "./src/index.tsx",
   },
   devtool: isDev ? "source-map" : false, //用于配置产物 Sourcemap 生成规则
   output: {
-    path: path.join(process.cwd(), "dist"),
+    path: path.join(process.cwd(), "docs"),
     filename: "[name].[contenthash].js", //入口代码块文件名的生成规则
     chunkFilename: "[name].[contenthash].js", //非入口模块的生成规则
     clean: true,
   },
   mode: process.env.NODE_ENV, //编译模式短语，支持 development、production 等值，可以理解为一种声明环境的短语
   resolve: {
-    // extensions: [".js", ".jsx", ".tsx", ".ts", '.png', '.jpg']
     modules: [path.resolve("node_modules")], // 解析第三方包
     extensions: [".js", ".ts", ".tsx", ".css", ".less", ".scss", ".json"], // 文件后缀名 先后顺序查找
     // mainFields: ['style', 'main'],// eg: bootstrap 先找package.json 的style字段 没有的话再找main字段
@@ -51,16 +52,16 @@ module.exports = {
   optimization: {
     // moduleIds: 'natural', named  deterministic size // 模块名称的生成规则 deterministic 生产模式默认值
     // chunkIds:'natural' // named  deterministic size //代码块名称的生成规则
-    // 自动分割第三方模块和公共模块
     // runtimeChunk: "single",
+    // 自动分割第三方模块和公共模块
     splitChunks: isDev
       ? false //关闭代码分包；
       : {
           chunks: "all", // 默认作用于异步chunk，值为 all 全部/initial同步/async异步
           minSize: 390 * 1024, //默认值是30kb，代码块的最小尺寸 超过这个尺寸的 Chunk 才会正式被分包；
           maxSize: 500 * 1024, //超过这个尺寸的 Chunk 会尝试进一步拆分出更小的 Chunk  设置 maxSize 的值会同时设置 maxAsyncSize 和 maxInitialSize 的值。
-          // maxAsyncSize: 500 * 1000, //与 maxSize 功能类似，但只对异步引入的模块生效；
-          // maxInitialSize: 500 * 1000 与 maxSize 类似，但只对 entry 配置的入口模块生效；
+          // maxAsyncSize: 500 * 1024, //与 maxSize 功能类似，但只对异步引入的模块生效；
+          // maxInitialSize: 500 * 1024 与 maxSize 类似，但只对 entry 配置的入口模块生效；
           minChunks: 2, //被多少模块共享，在分割之前模块的被引用次数
           maxAsyncRequests: 2, // 限制异步模块内部的并行最大请求数的，说白了你可以理解为是每个import()它里面的最大并行请求数量
           maxInitialRequests: 4, // 限制入口的拆分数量 用于设置 Initial Chunk 最大并行请求数；
@@ -85,6 +86,22 @@ module.exports = {
     minimize: isDev ? false : true, //关闭代码压缩;
     concatenateModules: isDev ? false : true, //关闭模块合并;
     usedExports: isDev ? false : true, //关闭 Tree-shaking 功能； // 标记使用到的导出
+    minimizer: [
+      // Webpack5 之后，约定使用 `'...'` 字面量保留默认 `minimizer` 配置
+      "...",
+      !isDev ? new CssMinimizerPlugin() : noop,
+      !isDev
+        ? new HtmlMinimizerPlugin({
+            minimizerOptions: {
+              // 折叠 Boolean 型属性
+              collapseBooleanAttributes: true,
+              // 使用精简 `doctype` 定义
+              useShortDoctype: true,
+              // ...
+            },
+          })
+        : noop,
+    ],
   },
   watchOptions: {
     ignored: /node_modules/, //最小化 watch 监控范围
@@ -113,15 +130,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.html$/,
+        test: /\.html$/i,
         use: [
           {
             loader: "html-loader",
-            options: { minimize: true },
           },
         ],
       },
-
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         type: "asset/resource",
@@ -129,9 +144,22 @@ module.exports = {
           {
             loader: "image-webpack-loader",
             options: {
-              // jpeg 压缩配置
               mozjpeg: {
+                // jpeg 压缩配置
                 quality: 80,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
               },
               disable: isDev ? true : false,
             },
@@ -170,6 +198,20 @@ module.exports = {
       },
     ],
   },
+  performance: !isDev //监控产物体积
+    ? {
+        // 设置所有产物体积阈值
+        maxAssetSize: 172 * 1024,
+        // 设置 entry 产物体积阈值
+        maxEntrypointSize: 244 * 1024,
+        // 报错方式，支持 `error` | `warning` | false
+        hints: "warning",
+        // 过滤需要监控的文件类型
+        assetFilter: function (assetFilename) {
+          return assetFilename.endsWith(".js");
+        },
+      }
+    : false,
   stats: "errors-only", // 只在错误时输出
   plugins: [
     // fork 出子进程，专门用于执行类型检查 这样，既可以获得 Typescript 静态类型检查能力，又能提升整体编译速度。
@@ -190,13 +232,12 @@ module.exports = {
       AUTHOR: JSON.stringify("yanyunchangfeng"),
     }),
     new FriendlyErrorsWebpackPlugin(), // .日志太多太少都不美观// .可以修改stats
-
     !isDev
       ? new CopyPlugin({
           patterns: [
             {
               from: path.resolve(process.cwd(), "src", "assets"),
-              to: path.resolve(process.cwd(), "dist"),
+              to: path.resolve(process.cwd(), "docs"),
             },
           ],
           options: {
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,21 @@
 const commonConfig = require("./webpack.common");
 const path = require("path");
 const { merge } = require("webpack-merge");
-const TerserJSPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(commonConfig, {
   optimization: {
-    minimizer: [new TerserJSPlugin({})],
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          minify: TerserPlugin.swcMinify, // TerserPlugin.esbuildMinify,
+          compress: {
+            reduce_vars: true,
+            pure_funcs: ["console.log"],
+          },
+          // ...
+        },
+      }),
+    ],
   },
   cache: {
     type: "filesystem", // memory filesystem,  // 默认是在内存中存储
