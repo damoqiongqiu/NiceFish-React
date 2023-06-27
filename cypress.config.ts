f31367861f5c5cfaf91929bd3c15418673f950c8
yanyunchangfeng <yanyunchangfeng@gmail.com>
2023-06-27 11:56:45 +0800

    feat: optimization deploy workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
-    defaultCommandTimeout: 20000,
+    defaultCommandTimeout: 25000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
