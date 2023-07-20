f3663b64a97391c4db92d0f0f288b7343828a36a
大漠穷秋 <damoqiongqiu@126.com>
2023-07-20 13:54:34 +0800

    format-code

--- src/containers/home/online-contact/index.tsx
+++ src/containers/home/online-contact/index.tsx
@@ -9,59 +9,10 @@ const OnlineContact: FC = () => {
         <div className="list-group">
           <a
             target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=8db5ed802cbddbf6432d7ba7dc4f2a316be020442491eb41cbfb1a12434e8cc7"
-            className="list-group-item  border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-1区:286047042
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=cbfcd79e7e90939b0e2c519f475fac4792985ce2abc5ad45ec5e06ffcfe944dd"
-            className="list-group-item  border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-2区:139357161
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=639229c8b6ad0c3a9a8f381dddf5d7785780b20d8c37eb25c91ac73ea7d37a5f"
-            className="list-group-item border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-3区:473129930
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=12add102af3f67910bdc0de753dee10ebada08ab485af7e38f4dfa0ee27476f7"
-            className="list-group-item border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-4区:483016484
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=1293a6494fb306ea29d281e320a8f4ef82285fa5300f73118e6ff7a79ce76036"
-            className="list-group-item border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-5区:604253120
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=fcd880ba919983dc85690642d48cf00ad0affd8d35de5f30542c895e622a8ab8"
-            className="list-group-item border-width-1-0 pd-lr-1rem"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> Angular-6区:124641447
-          </a>
-          <a
-            target="_blank"
             href="//shang.qq.com/wpa/qunwpa?idkey=5d6b8c5296e4806142b8422ae7abca6f27b9b9b992a4dac80dc1392644e8970a"
             className="list-group-item border-width-1-0 pd-lr-1rem"
           >
             <i className="fa fa-qq" aria-hidden="true" /> 脚本娃娃-桃花岛:83163037
-          </a>
-          <a
-            target="_blank"
-            href="//shang.qq.com/wpa/qunwpa?idkey=cbf15a3d8f212076a8de5b6fa4b5a16d750cc5d7f2d55b14ad0ea8bcb6fd976e"
-            className="list-group-item border-width-1-0 pd-lr-1rem border-bottom-0"
-          >
-            <i className="fa fa-qq" aria-hidden="true" /> React 全家福-1区:701149807
           </a>
         </div>
       </div>
