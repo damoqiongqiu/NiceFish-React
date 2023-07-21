069ce583f54622d56d5b852a9a3c6dd0c7199b39
大漠穷秋 <damoqiongqiu@126.com>
2023-07-21 15:11:09 +0800

    路由配置拆分成独立的文件，重写渲染路由的函数

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,167 +3,10 @@ import Home from 'src/app/blog/home';
 import Exception404 from 'src/app/utils/exception/404';
 import ErrorBoundary from './utils/ErrorBoundary';
 import { HashRouter as Router, Route, Routes, Navigate, NavLink } from 'react-router-dom';
+import renderRoutes from 'src/app/routes';
 
 import niceFishPNG from 'src/assets/images/nice-fish.png';
 import './index.scss';
-
-const Manage = lazy(() => import(/*webpackChunkName:'manage,webpackPrefetch:true'*/ './manage'));
-const SignIn = lazy(() => import(/*webpackChunkName:'sign-in'*/ './blog/user/sign-in'));
-const SignUp = lazy(() => import(/*webpackChunkName:'sign-up'*/ './blog/user/sign-up'));
-const RetrievePwd = lazy(() => import(/*webpackChunkName:'retrieve-pwd'*/ './blog/user/retrieve-pwd'));
-const Write = lazy(() => import(/*webpackChunkName:'write', webpackPrefetch:true*/ './blog/write'));
-const PostTable = lazy(() => import(/*webpackChunkName:'post-table'*/ './manage/content-mng/post-table'));
-const Chart = lazy(() => import(/*webpackChunkName:'chart',webpackPrefetch:true*/ './manage/chart'));
-const CommentTable = lazy(() => import(/*webpackChunkName:'comment-table'*/ './manage/content-mng/comment-table'));
-const Profile = lazy(() => import(/*webpackChunkName:'profile'*/ './manage/permission/user-profile'));
-const RoleTable = lazy(() => import(/*webpackChunkName:'role-page'*/ './manage/permission/role-table'));
-const RoleEdit = lazy(() => import(/*webpackChunkName:'role-page'*/ './manage/permission/role-edit'));
-const SysParam = lazy(() => import(/*webpackChunkName:'sys-param'*/ './manage/sys-param'));
-const ApiPermissionTable = lazy(() => import(/*webpackChunkName:'api-permission-table'*/ './manage/permission/api-permission-table'));
-const ApiPermissionEdit = lazy(() => import(/*webpackChunkName:'api-permission-edit'*/ './manage/permission/api-permission-edit'));
-const ComponentPermissionTable = lazy(() => import(/*webpackChunkName:'component-permission-table'*/ './manage/permission/component-permission-table'));
-const ComponentPermissionEdit = lazy(() => import(/*webpackChunkName:'component-permission-edit'*/ './manage/permission/component-permission-edit'));
-const UserTable = lazy(() => import(/*webpackChunkName:'user-table'*/ './manage/permission/user-table'));
-const PostDetailMain = lazy(() => import(/*webpackChunkName:'post-detail-main'*/ './blog/read/post-detail-main'));
-
-const routes = [
-  {
-    path: '/',
-    element: Navigate,
-    redirect: '/post'
-  },
-  {
-    path: '/post',
-    element: Home
-  },
-  {
-    path: '/post/post-detail/:id',
-    element: PostDetailMain
-  },
-  {
-    path: '/home',
-    element: Home
-  },
-  {
-    path: '/manage',
-    element: Manage,
-    routes: [
-      {
-        path: 'chart',
-        element: Chart
-      },
-      {
-        path: 'post-table',
-        element: PostTable
-      },
-      {
-        path: 'comment-table',
-        element: CommentTable
-      },
-      {
-        path: 'profile',
-        element: Profile
-      },
-      {
-        path: 'user-table',
-        element: UserTable
-      },
-      {
-        path: 'user-table/edituser/:userId',
-        element: Profile
-      },
-      {
-        path: 'role-table',
-        element: RoleTable
-      },
-      {
-        path: 'role-edit/:roleId',
-        element: RoleEdit
-      },
-      {
-        path: 'permission/api-permission-table',
-        element: ApiPermissionTable
-      },
-      {
-        path: 'permission/api-permission-edit/:permissionId',
-        element: ApiPermissionEdit
-      },
-      {
-        path: 'permission/component-permission-table',
-        element: ComponentPermissionTable
-      },
-      {
-        path: 'permission/component-permission-edit/:compPermId',
-        element: ComponentPermissionEdit
-      },
-      {
-        path: 'sysparam',
-        element: SysParam
-      }
-    ]
-  },
-  {
-    path: '/sign-in',
-    element: SignIn
-  },
-  {
-    path: '/retrieve-pwd',
-    element: RetrievePwd
-  },
-  {
-    path: '/sign-up',
-    element: SignUp
-  },
-  {
-    path: '/write',
-    element: Write
-  },
-  {
-    path: '*',
-    element: Exception404
-  }
-];
-
-const renderSubRoute = (route) => {
-  return (
-    <Route
-      key={route.path}
-      path={route.path}
-      element={
-        route.redirect ? (
-          <>
-            <Navigate to={`${route.redirect}`} replace />
-          </>
-        ) : (
-          <route.element />
-        )
-      }
-    >
-      {renderRoute(route.routes)}
-    </Route>
-  );
-};
-
-const renderRoute = (routes) => {
-  return routes.map((route) => {
-    if (route.routes) return renderSubRoute(route);
-    return (
-      <Route
-        key={route.path}
-        path={route.path}
-        element={
-          route.redirect ? (
-            <>
-              <Navigate to={`${route.redirect}`} replace />
-            </>
-          ) : (
-            <route.element />
-          )
-        }
-      ></Route>
-    );
-  });
-};
 
 export default props => {
   return (
@@ -229,7 +72,7 @@ export default props => {
               </div>
             }
           >
-            <Routes>{renderRoute(routes)}</Routes>
+            <Routes>{renderRoutes()}</Routes>
           </Suspense>
         </ErrorBoundary>
       </div>
---
+++ src/app/routes.js
@@ -0,0 +1,159 @@
+import React, { lazy } from "react";
+import Home from "src/app/blog/home";
+import Exception404 from "src/app/utils/exception/404";
+import { Route, Navigate } from "react-router-dom";
+
+import "./index.scss";
+
+const Manage = lazy(() => import(/*webpackChunkName:'manage,webpackPrefetch:true'*/ "./manage"));
+const SignIn = lazy(() => import(/*webpackChunkName:'sign-in'*/ "./blog/user/sign-in"));
+const SignUp = lazy(() => import(/*webpackChunkName:'sign-up'*/ "./blog/user/sign-up"));
+const RetrievePwd = lazy(() => import(/*webpackChunkName:'retrieve-pwd'*/ "./blog/user/retrieve-pwd"));
+const Write = lazy(() => import(/*webpackChunkName:'write', webpackPrefetch:true*/ "./blog/write"));
+const PostTable = lazy(() => import(/*webpackChunkName:'post-table'*/ "./manage/content-mng/post-table"));
+const Chart = lazy(() => import(/*webpackChunkName:'chart',webpackPrefetch:true*/ "./manage/chart"));
+const CommentTable = lazy(() => import(/*webpackChunkName:'comment-table'*/ "./manage/content-mng/comment-table"));
+const Profile = lazy(() => import(/*webpackChunkName:'profile'*/ "./manage/permission/user-profile"));
+const RoleTable = lazy(() => import(/*webpackChunkName:'role-page'*/ "./manage/permission/role-table"));
+const RoleEdit = lazy(() => import(/*webpackChunkName:'role-page'*/ "./manage/permission/role-edit"));
+const SysParam = lazy(() => import(/*webpackChunkName:'sys-param'*/ "./manage/sys-param"));
+const ApiPermissionTable = lazy(() =>
+    import(/*webpackChunkName:'api-permission-table'*/ "./manage/permission/api-permission-table")
+);
+const ApiPermissionEdit = lazy(() =>
+    import(/*webpackChunkName:'api-permission-edit'*/ "./manage/permission/api-permission-edit")
+);
+const ComponentPermissionTable = lazy(() =>
+    import(/*webpackChunkName:'component-permission-table'*/ "./manage/permission/component-permission-table")
+);
+const ComponentPermissionEdit = lazy(() =>
+    import(/*webpackChunkName:'component-permission-edit'*/ "./manage/permission/component-permission-edit")
+);
+const UserTable = lazy(() => import(/*webpackChunkName:'user-table'*/ "./manage/permission/user-table"));
+const PostDetailMain = lazy(() => import(/*webpackChunkName:'post-detail-main'*/ "./blog/read/post-detail-main"));
+
+const routes = [
+    {
+        path: "/",
+        element: Navigate,
+        redirect: "/post",
+    },
+    {
+        path: "/post",
+        element: Home,
+    },
+    {
+        path: "/post/post-detail/:id",
+        element: PostDetailMain,
+    },
+    {
+        path: "/home",
+        element: Home,
+    },
+    {
+        path: "/manage",
+        element: Manage,
+        routes: [
+            {
+                path: "chart",
+                element: Chart,
+            },
+            {
+                path: "post-table",
+                element: PostTable,
+            },
+            {
+                path: "comment-table",
+                element: CommentTable,
+            },
+            {
+                path: "profile",
+                element: Profile,
+            },
+            {
+                path: "user-table",
+                element: UserTable,
+            },
+            {
+                path: "user-table/edituser/:userId",
+                element: Profile,
+            },
+            {
+                path: "role-table",
+                element: RoleTable,
+            },
+            {
+                path: "role-edit/:roleId",
+                element: RoleEdit,
+            },
+            {
+                path: "permission/api-permission-table",
+                element: ApiPermissionTable,
+            },
+            {
+                path: "permission/api-permission-edit/:permissionId",
+                element: ApiPermissionEdit,
+            },
+            {
+                path: "permission/component-permission-table",
+                element: ComponentPermissionTable,
+            },
+            {
+                path: "permission/component-permission-edit/:compPermId",
+                element: ComponentPermissionEdit,
+            },
+            {
+                path: "sysparam",
+                element: SysParam,
+            },
+        ],
+    },
+    {
+        path: "/sign-in",
+        element: SignIn,
+    },
+    {
+        path: "/retrieve-pwd",
+        element: RetrievePwd,
+    },
+    {
+        path: "/sign-up",
+        element: SignUp,
+    },
+    {
+        path: "/write",
+        element: Write,
+    },
+    {
+        path: "*",
+        element: Exception404,
+    },
+];
+
+const doRenderRoutes = (routes) => {
+    return routes.map((route) => {
+        return (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={
+                    route.redirect ? (
+                        <>
+                            <Navigate to={`${route.redirect}`} replace />
+                        </>
+                    ) : (
+                        <route.element />
+                    )
+                }
+            >
+                {route.routes && doRenderRoutes(route.routes)}
+            </Route>
+        );
+    });
+};
+
+const renderRoutes = () => {
+    return doRenderRoutes(routes);
+};
+
+export default renderRoutes;
