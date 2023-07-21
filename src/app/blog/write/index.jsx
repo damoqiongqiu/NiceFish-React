76bf321084551736a489249b87299ce3afe69cfb
大漠穷秋 <damoqiongqiu@126.com>
2023-07-21 14:10:23 +0800

    重构写文章的页面

--- src/app/blog/write/index.jsx
+++ src/app/blog/write/index.jsx
@@ -1,25 +1,47 @@
 import React, { FC, useState, useEffect } from 'react';
-import { Editor } from 'primereact/editor';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export default props=> {
+import './index.scss';
+
+export default props => {
   const [text, setText] = useState('');
 
   return (
-    <div className="container-xl write-post-container mtb-16px">
-      <div className="row no-gutters">
+    <div className="container write-post-container">
+      <div className="row">
         <div className="col-md-12">
-          <form role="form">
+          <form role="form" noValidate>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="标题，2到32个字符" />
+              <input
+                required minLength="2" maxLength="32"
+                name="title"
+                type="text"
+                className="form-control"
+                placeholder="标题，2到32个字符" />
             </div>
-            <div className="form-group border-1px-primary">
-              <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
+            <div className="form-group">
+              <CKEditor
+                editor={ClassicEditor}
+                // data="<p>Hello from CKEditor 5!</p>"
+                onReady={editor => {
+                  // You can store the "editor" and use when it is needed.
+                  console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  console.log({ event, editor, data });
+                  //setText()???
+                }}
+                onBlur={(event, editor) => {
+                  console.log('Blur.', editor);
+                }}
+                onFocus={(event, editor) => {
+                  console.log('Focus.', editor);
+                }}
+              />
             </div>
-            <div className="pd-16px">
-              <button type="button" className="btn btn-primary">
-                提交
-              </button>
-            </div>
+            <button type="submit" className="btn btn-primary">提交</button>
           </form>
         </div>
       </div>
