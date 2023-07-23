59bfed9d2541cb9e73523f8cd0bf5d9f1c11690b
大漠穷秋 <damoqiongqiu@126.com>
2023-07-23 17:31:24 +0800

    post detail 页面对接 mock 数据

--- src/app/blog/post/post-detail/index.jsx
+++ src/app/blog/post/post-detail/index.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import headImgNarrow from 'src/assets/images/headline-narrow.png';
+import postService from 'src/app/blog/post/post-service';
 import './index.scss';
 
-//mock-data
-import post from "src/mock-data/post-detail-mock.json";
-
 export default props => {
+  const [postDetail, setPostDetail] = useState([]);
+  useEffect(() => {
+    postService.getPostDetail(1).then(response => {
+      console.log(response);
+      let data = response.data;
+      setPostDetail(data);
+    });
+  }, []);
+
   return (
     <div className="post-detail-container">
       <div className="img-container">
@@ -13,25 +20,25 @@ export default props => {
       </div>
       <div className="row">
         <div className="col-md-12">
-          <h3>{post.title}</h3>
+          <h3>{postDetail.title}</h3>
         </div>
       </div>
       <div className="row post-info">
         <div className="col-md-3">
-          <span className="fa fa-user"></span>{post.nickName}
+          <span className="fa fa-user"></span>{postDetail.nickName}
         </div>
         <div className="col-md-3">
-          <span className="fa fa-clock-o"></span>{post.postTime}
+          <span className="fa fa-clock-o"></span>{postDetail.postTime}
         </div>
         <div className="col-md-3">
-          <span className="fa fa-hand-pointer-o"></span>点击：{post.readTimes}
+          <span className="fa fa-hand-pointer-o"></span>点击：{postDetail.readTimes}
         </div>
         <div className="col-md-3">
-          <span className="fa fa-comment"></span>评论：{post.commentTimes}
+          <span className="fa fa-comment"></span>评论：{postDetail.commentTimes}
         </div>
       </div>
       <div className="row">
-        <div className="col-md-12 post-content">{post.content}
+        <div className="col-md-12 post-content">{postDetail.content}
         </div>
       </div>
     </div >
--- src/app/blog/post/post-service/index.js
+++ src/app/blog/post/post-service/index.js
@@ -1,7 +1,9 @@
 import axiosService from "src/app/utils/nicefish.axios.service";
+import * as _ from 'lodash';
 import environment from "src/environments/environment";
 
 const postListURL = environment.dataURL.postListURL;
+const postDetailURL = environment.dataURL.postDetailURL;
 
 export default {
     getPostList: (page) => {
@@ -10,5 +12,10 @@ export default {
                 id: 12345
             }
         });
+    },
+    getPostDetail(id) {
+        let reqURL = _.template(postDetailURL)({ id: id });
+        console.log(reqURL);
+        return axiosService.get(reqURL);
     }
 }
