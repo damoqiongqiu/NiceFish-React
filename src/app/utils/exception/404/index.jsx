a7b1516d8b277b19319890845d29df55e0643825
大漠穷秋 <damoqiongqiu@126.com>
2023-07-27 11:58:09 +0800

    修改404页面样式

--- src/app/utils/exception/404/index.jsx
+++ src/app/utils/exception/404/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import img404 from 'src/assets/images/404.svg';
 
 const Exception404 = () => {
   return (
@@ -9,6 +10,9 @@ const Exception404 = () => {
         <div className="col-4 font-size-24 text-secondary">
           <h1 className="font-size-48">404</h1>
           <div>抱歉，你访问的页面不存在</div>
+          <div>
+            <img src={img404} alt="" />
+          </div>
           <div className="mt-16px">
             <NavLink to="/">
               <button type="button" className="btn btn-primary">
