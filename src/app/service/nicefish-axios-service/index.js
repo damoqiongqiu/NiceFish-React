32a1b565d6303adcd430114e4b8b8c554b209f59
大漠穷秋 <damoqiongqiu@126.com>
2023-07-29 13:05:47 +0800

    nicefish-axios-service 添加响应消息 mapping

--- src/app/service/nicefish-axios-service/index.js
+++ src/app/service/nicefish-axios-service/index.js
@@ -19,6 +19,19 @@ axiosService.interceptors.request.use(
     }
 );
 
+const errorMsgMap = {
+    400: '请求参数错误',
+    401: '当前操作没有权限',
+    403: '权限不足,请联系管理员',
+    404: '请求地址不存在',
+    405: '不支持该请求类型',
+    408: '请求超时',
+    500: '服务器内部错误',
+    501: '服务未实现',
+    503: '服务不可用',
+    504: '后端服务异常',
+    600: '网络异常，请稍后重试',
+}
 axiosService.interceptors.response.use(
     (response) => {
         window.hideGlobalSpin();
@@ -27,9 +40,9 @@ axiosService.interceptors.response.use(
     (error) => {
         window.hideGlobalSpin();
         console.error(error);
-        //TODO:发现 session 超时前端做退出动作，删掉浏览器缓存，跳转到首页。
-        let errorMsg = `Error Code: ${error.response.status},  Message: ${error.message}`;
+        let errorMsg = errorMsgMap[error?.response?.status] || `Error Code: ${error.response.status},  Message: ${error.message}`;
         console.error(errorMsg);
+        //TODO:发现 session 超时前端做退出动作，删掉浏览器缓存，跳转到首页。
         niceFishToast({
             severity: 'error',
             summary: 'HTTP ERROR',
