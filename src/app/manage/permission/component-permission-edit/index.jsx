6ccb26fe2d11aa09878dce5ca9c97c5544ab7d83
大漠穷秋 <damoqiongqiu@126.com>
2023-08-13 13:42:11 +0800

    component-permission-edit 采用 ajv.js 进行表单校验

--- src/app/manage/permission/component-permission-edit/index.jsx
+++ src/app/manage/permission/component-permission-edit/index.jsx
@@ -2,8 +2,71 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import compPermService from "src/app/service/component-permission-service";
+import ajv from "src/app/service/ajv-validate-service";
 
 import './index.scss';
+
+// 表单输入项数据规格定义
+const schema = {
+  "type": "object",
+  "properties": {
+    "componentName": {
+      "type": "string",
+      "minLength": 2,
+      "maxLength": 32,
+      "errorMessage": "API 名称长度在 2 到 32 个字符之间。"
+    },
+    "icon": {
+      "anyOf": [
+        {
+          "type": "string",
+          "maxLength": 1024,
+          "errorMessage": "icon 最大长度 1024 个字符。"
+        },
+        { "type": "null" },
+        { "type": "string", "minLength": 0 } // 允许空字符串
+      ]
+    },
+    "url": {
+      "anyOf": [
+        {
+          "type": "string",
+          "maxLength": 1024,
+          "errorMessage": "url 最大长度 1024 个字符。"
+        },
+        { "type": "null" },
+        { "type": "string", "minLength": 0 } // 允许空字符串
+      ]
+    },
+    "displayOrder": {
+      "type": "number",
+      "minimum": 1,
+      "maximum": 100000,
+      "errorMessage": "数值范围在 1 到 10 万之间。"
+    },
+    "permission": {
+      "type": "string",
+      "minLength": 1,
+      "maxLength": 64,
+      "errorMessage": "权限字符串长度在 1 到 64 个字符之间。"
+    },
+    "remark": {
+      "anyOf": [
+        {
+          "type": "string",
+          "minLength": 2,
+          "maxLength": 200,
+          "errorMessage": "备注长度在 8 到 16 个字符之间。"
+        },
+        { "type": "null" },
+        { "type": "string", "minLength": 0 } // 允许空字符串
+      ]
+    },
+  },
+  "required": ["componentName", "displayOrder", "permission"],
+}
+//ajv 的 compile 吃资源较多，这里放在组件外面，保证只执行一次。
+const ajvValidate = ajv.compile(schema);
 
 export default props => {
   //导航对象
@@ -13,7 +76,7 @@ export default props => {
   const { compPermId, pId } = useParams();
 
   //表单校验错误信息
-  const [isFormValid, setFormValid] = useState(true);
+  const [errors, setErrors] = useState({});
 
   //formValue 里面的 k-v 与服务端接口对应，方便提交和加载数据。
   const [formValue, setFormValue] = useState({
@@ -29,230 +92,13 @@ export default props => {
   });
 
   /**
-   * 输入项的校验状态
-   */
-  const [validationResult, setValidationResult] = useState({
-    componentName: {
-      valid: true,
-      ruleName: "",//valid 为 true 时，此项为空
-      message: '',
-    },
-    icon: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    },
-    url: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    },
-    displayOrder: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    },
-    permission: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    },
-    visiable: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    },
-    remark: {
-      valid: true,
-      ruleName: "",
-      message: '',
-    }
-  });
-
-  /**
-   * 输入项的校验规则
-   */
-  const validators = {
-    componentName: [
-      {
-        ruleName: 'required',
-        message: '请输入 API 名称',
-        fn: (value) => {
-          return (value + "").trim().length > 0;
-        }
-      },
-      {
-        ruleName: 'maxLength',
-        message: 'API 名称最多 32 位',
-        fn: (value) => {
-          return (value + "").trim().length <= 32;
-        }
-      },
-      {
-        ruleName: 'minLength',
-        message: 'API 名称最少 2 位',
-        fn: (value) => {
-          return (value + "").trim().length >= 2;
-        }
-      }
-    ],
-    icon: [
-      {
-        ruleName: 'maxLength',
-        message: 'URL 最多 1024 个字符',
-        fn: (value) => {
-          return (value + "").trim().length <= 1024;
-        }
-      }
-    ],
-    url: [
-      {
-        ruleName: 'maxLength',
-        message: 'URL 最多 1024 个字符',
-        fn: (value) => {
-          return (value + "").trim().length <= 1024;
-        }
-      }
-    ],
-    displayOrder: [
-      {
-        ruleName: 'required',
-        message: '请输入显示顺序',
-        fn: (value) => {
-          return (value + "").trim().length > 0;
-        }
-      },
-      {
-        ruleName: 'maxLength',
-        message: '显示顺序最多 4 位',
-        fn: (value) => {
-          return (value + "").trim().length <= 4;
-        }
-      },
-      {
-        ruleName: 'minLength',
-        message: '显示顺序最少 1 位',
-        fn: (value) => {
-          return (value + "").trim().length >= 1;
-        }
-      }
-    ],
-    permission: [
-      {
-        ruleName: 'required',
-        message: '请输入权限通配符',
-        fn: (value) => {
-          return (value + "").trim().length > 0;
-        }
-      },
-      {
-        ruleName: 'maxLength',
-        message: '权限通配符最多 64 位',
-        fn: (value) => {
-          return (value + "").trim().length <= 64;
-        }
-      },
-      {
-        ruleName: 'minLength',
-        message: '权限通配符最少 1 位',
-        fn: (value) => {
-          return (value + "").trim().length >= 1;
-        }
-      }
-    ],
-    remark: [
-      {
-        ruleName: 'maxLength',
-        message: '备注最多 200 个字符',
-        fn: (value) => {
-          return (value + "").trim().length <= 200;
-        }
-      },
-      {
-        ruleName: 'minLength',
-        message: '备注最少 2 个字符',
-        fn: (value) => {
-          return (value + "").trim().length >= 2;
-        }
-      }
-    ],
-  }
-
-  /**
-   * 校验单个输入项的合法性
-   */
-  const validateField = (name, value) => {
-    if (!validators[name]) {
-      return;
-    }
-
-    let temp = {
-      ...validationResult,
-      ...{
-        [name]: {
-          valid: true,
-          ruleName: "",
-          message: '',
-        }
-      }
-    };
-
-    //非必填且值为空时，结果标记为合法，不再继续校验。
-    if (value.length === 0) {
-      let isRequired = false;
-      validators[name].forEach(validator => {
-        if (validator.ruleName === 'required') {
-          isRequired = true;
-        }
-      });
-      if (!isRequired) {
-        return;
-      }
-    }
-
-    validators[name].forEach(validator => {
-      if (!validator.fn(value)) {
-        temp[name].valid = false;
-        temp[name].ruleName = validator.ruleName;
-        temp[name].message = validator.message;
-      }
-    });
-
-    setValidationResult(temp);
-  }
-
-  /**
    * 所有 input 的 onChange 事件的处理函数，对于 checkbox/radio/select 这些组件，需要处理好 value 值再调用此函数。
    */
   const handleInputChange = (name, value) => {
-    validateField(name, value);
     setFormValue({
       ...formValue,
       [name]: value
     });
-  }
-
-  /**
-   * 校验表单整体的合法性，只要有一个输入项不合法，表单整体标记为不合法。
-   * @returns 
-   */
-  const validateFormAll = () => {
-    let flag = true;
-
-    for (let key in formValue) {
-      if (document.getElementsByName(key).length) {
-        let value = document.getElementsByName(key)[0].value;
-        validateField(key, value);
-      }
-    }
-
-    for (let key in validationResult) {
-      if (!validationResult[key].valid) {
-        flag = false;
-      }
-    }
-
-    setFormValid(flag);
   }
 
   /**
@@ -263,13 +109,17 @@ export default props => {
   const save = (e) => {
     e.preventDefault();
 
-    validateFormAll();//FIXME:校验逻辑看起来没有生效。
-    if (!isFormValid) {
-      niceFishToast({
-        severity: 'error',
-        summary: 'Error',
-        detail: '存在不合法的输入项，请检查',
+    const isValid = ajvValidate(formValue);
+    setErrors({});
+
+    if (!isValid) {
+      const fieldErrors = {};
+      ajvValidate?.errors.forEach((error) => {
+        const field = error.instancePath.substring(1);
+        fieldErrors[field] = error.message;
       });
+      setErrors(fieldErrors);
+      console.log(fieldErrors);
       return;
     }
 
@@ -399,7 +249,7 @@ export default props => {
                 }
               </div>
             </div>
-            <div className=" form-group">
+            <div className={`form-group  ${errors.componentName ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">组件名称：</label>
               <div className="col-md-10">
                 <input
@@ -409,11 +259,13 @@ export default props => {
                   name="componentName"
                   value={formValue.componentName}
                   onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
                 />
+                {
+                  errors.componentName ? <div className="text-danger">{errors.componentName}</div> : <></>
+                }
               </div>
             </div>
-            <div className="form-group">
+            <div className={`form-group  ${errors.icon ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">组件图标：</label>
               <div className="col-md-10">
                 <input
@@ -423,11 +275,13 @@ export default props => {
                   name="icon"
                   value={formValue.icon || ""}
                   onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
                 />
+                {
+                  errors.icon ? <div className="text-danger">{errors.icon}</div> : <></>
+                }
               </div>
             </div>
-            <div className="form-group">
+            <div className={`form-group  ${errors.url ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">组件 URL：</label>
               <div className="col-md-10">
                 <input
@@ -437,11 +291,13 @@ export default props => {
                   name="url"
                   value={formValue.url || ""}
                   onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
                 />
+                {
+                  errors.url ? <div className="text-danger">{errors.url}</div> : <></>
+                }
               </div>
             </div>
-            <div className="form-group">
+            <div className={`form-group  ${errors.displayOrder ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">显示顺序：</label>
               <div className="col-md-10">
                 <input
@@ -450,12 +306,14 @@ export default props => {
                   placeholder="请输入显示顺序"
                   name="displayOrder"
                   value={formValue.displayOrder}
-                  onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
+                  onChange={(e) => handleInputChange(e.target.name, parseInt(e.target.value))}
                 />
+                {
+                  errors.displayOrder ? <div className="text-danger">{errors.displayOrder}</div> : <></>
+                }
               </div>
             </div>
-            <div className="form-group">
+            <div className={`form-group  ${errors.permission ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">权限通配符：</label>
               <div className="col-md-10">
                 <input
@@ -465,8 +323,10 @@ export default props => {
                   name="permission"
                   value={formValue.permission}
                   onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
                 />
+                {
+                  errors.permission ? <div className="text-danger">{errors.permission}</div> : <></>
+                }
                 <p className="bg-danger">
                   Apache Shiro 通配符权限文档： https://shiro.apache.org/permissions.html
                 </p>
@@ -490,7 +350,7 @@ export default props => {
                 </div>
               </div>
             </div>
-            <div className="form-group">
+            <div className={`form-group  ${errors.remark ? "has-error" : ""}`}>
               <label className="col-md-2 control-label">备注：</label>
               <div className="col-md-10">
                 <textarea
@@ -501,9 +361,11 @@ export default props => {
                   name="remark"
                   value={formValue.remark}
                   onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-                  onBlur={(e) => validateField(e.target.name, e.target.value)}
                 >
                 </textarea>
+                {
+                  errors.remark ? <div className="text-danger">{errors.remark}</div> : <></>
+                }
               </div>
             </div>
             <div className="form-group">
