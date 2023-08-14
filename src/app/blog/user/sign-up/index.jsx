61df02407ed50c77124f3e8a9dbcf3a39d778ba4
大漠穷秋 <damoqiongqiu@126.com>
2023-08-14 14:54:46 +0800

    注册页面采用 i18n 字符串

--- src/app/blog/user/sign-up/index.jsx
+++ src/app/blog/user/sign-up/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import userService from 'src/app/service/user-service';
 import ajv from "src/app/service/ajv-validate-service";
-
 import './index.scss';
 
 // 表单输入项数据规格定义
@@ -12,13 +12,11 @@ const schema = {
     "email": {
       "type": "string",
       "format": 'email',
-      "errorMessage": "请输入合法的邮箱格式。"
     },
     "password": {
       "type": "string",
       "minLength": 8,
       "maxLength": 16,
-      "errorMessage": "密码长度在 8 到 16 个字符之间。"
     },
     "confirmPassword": {
       "const": {
@@ -27,7 +25,6 @@ const schema = {
       "type": "string",
       "minLength": 8,
       "maxLength": 16,
-      "errorMessage": "两次密码必须相同。"
     },
   },
   "required": ["email", "password", "confirmPassword"],
@@ -36,6 +33,9 @@ const schema = {
 const ajvValidate = ajv.compile(schema);
 
 export default props => {
+  //i18n hooks
+  const { i18n } = useTranslation();
+
   //导航对象
   const navigate = useNavigate();
 
@@ -82,10 +82,15 @@ export default props => {
 
     if (!isValid) {
       const fieldErrors = {};
-      ajvValidate?.errors.forEach((error) => {
+
+      ajvValidate.errors.forEach((error) => {
         const field = error.instancePath.substring(1);
-        fieldErrors[field] = error.message;
+        const keyword = error.keyword;
+        // 获取 i8n 中的错误信息，如果没有则使用默认的错误信息。i18n 字符串定义在 src\app\shared\i18n\ 中。
+        const errorMessage = i18n.t(`validation.${keyword}`, error.params);
+        fieldErrors[field] = errorMessage || error.message;;
       });
+
       setErrors(fieldErrors);
       console.log(fieldErrors);
       return;
@@ -99,14 +104,14 @@ export default props => {
           niceFishToast({
             severity: 'success',
             summary: 'Success',
-            detail: '创建成功',
+            detail: i18n.t("success"),
           });
           navigate('/sign-in');
         } else {
           niceFishToast({
             severity: 'error',
             summary: 'Error',
-            detail: data?.msg || '创建失败',
+            detail: data?.msg || i18n.t("fail"),
           });
         }
       },
@@ -114,7 +119,7 @@ export default props => {
         niceFishToast({
           severity: 'error',
           summary: 'Error',
-          detail: '创建失败',
+          detail: i18n.t("fail"),
         });
       }
     );
@@ -124,12 +129,12 @@ export default props => {
     <div className="user-register-container">
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">Sign Up</h3>
+          <h3 className="panel-title">{i18n.t("signUp")}</h3>
         </div>
         <div className="panel-body">
           <form className="form-horizontal" role="form" noValidate onSubmit={(e) => doRegister(e)}>
             <div className={`form-group  ${errors.email ? "has-error" : ""}`}>
-              <label className="col-md-3 control-label">Email:</label>
+              <label className="col-md-3 control-label">{i18n.t("email")}:</label>
               <div className="col-md-9">
                 <input
                   className="form-control"
@@ -144,7 +149,7 @@ export default props => {
               </div>
             </div>
             <div className={`form-group ${errors.password ? "has-error" : ""}`}>
-              <label className="col-md-3 control-label">Password:</label>
+              <label className="col-md-3 control-label">{i18n.t("password")}:</label>
               <div className="col-md-9">
                 <input
                   className="form-control"
@@ -159,7 +164,7 @@ export default props => {
               </div>
             </div>
             <div className={`form-group  ${errors.confirmPassword ? "has-error" : ""}`}>
-              <label className="col-md-3 control-label">Confirm Password:</label>
+              <label className="col-md-3 control-label">{i18n.t("confirmPassword")}:</label>
               <div className="col-md-9">
                 <input
                   className="form-control"
@@ -175,7 +180,7 @@ export default props => {
             </div>
             <div className="form-group">
               <div className="col-md-offset-3 col-md-9">
-                <button type="button" onClick={doSignUp} className="btn btn-success">Sign Up</button>
+                <button type="button" onClick={doSignUp} className="btn btn-success">{i18n.t("signUp")}</button>
               </div>
             </div>
           </form>
