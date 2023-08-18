0a5e2bd3d53065e53d9efdc9a44a93b30dd095b3
大漠穷秋 <damoqiongqiu@126.com>
2023-08-18 21:01:20 +0800

    修改 shared/i18n ，解决 i18next-http-backend 加载 json 字符串的路径问题

--- src/app/shared/i18n/index.js
+++ src/app/shared/i18n/index.js
@@ -2,6 +2,20 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import environment from "src/environments/environment";
+
+// 获取当前页面的协议、域名和端口号
+const protocol = window.location.protocol;
+const hostname = window.location.hostname;
+const port = window.location.port;
+
+//开发态默认指向根目录，构建指向 /NiceFish-React/ ，请根据项目情况修改这里的值。
+const baseUrl = !environment.production
+    ? '/'
+    : `${protocol}//${hostname}${port ? ':' + port : ''}/NiceFish-React/`;
+
+// 构建加载路径
+const baseLoadPath = `${baseUrl}locales/{{lng}}/translation.json`;
 
 i18n
     .use(Backend)
@@ -13,7 +27,10 @@ i18n
         debug: true,
         interpolation: {
             escapeValue: false,
-        }
+        },
+        backend: {
+            loadPath: baseLoadPath,
+        },
     });
 
 export default i18n;
