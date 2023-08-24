82ae6b0ab6ee979c37c1fba625644cc65b68e9fe
大漠穷秋 <damoqiongqiu@126.com>
2023-08-24 17:03:16 +0800

    重构 UserHome 组件，抽出 UserInfo 组件’

--- src/app/blog/user/user-home/index.jsx
+++ src/app/blog/user/user-home/index.jsx
@@ -2,30 +2,15 @@ import React, { useEffect, useState } from 'react';
 import Masonry from 'react-masonry-css';
 import { TabView, TabPanel } from 'primereact/tabview';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import userService from 'src/app/service/user-service';
 import postService from 'src/app/service/post-service';
 import PostListItem from 'src/app/blog/post/post-list-item';
+import UserInfoHorizontal from "src/app/blog/user/user-info-horizontal";
 import './index.scss';
 
 const UserHome = (props) => {
     //userId ，从路由参数中获取
     const { userId } = useParams();
-
-    //用户详情
-    const [userDetail, setUserDetail] = useState({});
-
-    //粉丝数
-    const [followerCount, setFollowerCount] = useState(0);
-
-    //关注数
-    const [followingCount, setFollowingCount] = useState(0);
-
-    //获赞数
-    const [likedCount, setLikedCount] = useState(0);
-
-    //从 redux 中获取当前登录用户
-    const sessionUser = useSelector((state) => state.session.user) || {};
 
     //导航对象
     const navigate = useNavigate();
@@ -42,23 +27,7 @@ const UserHome = (props) => {
     const [page, setPage] = useState(1);
     const [totalElements, setTotalElements] = useState(0);
 
-    //是否已经关注
-    const [followed, setFollowed] = useState(false);
-
     useEffect(() => {
-        userService.getUserFollowerCount(userId).then(response => {
-            setFollowerCount(response.data);
-        });
-        userService.getUserFollowingCount(userId).then(response => {
-            setFollowingCount(response.data);
-        });
-        userService.getUserLikedCount(userId).then(response => {
-            setLikedCount(response.data);
-        });
-        userService.getUserDetails(userId).then(response => {
-            setUserDetail(response.data.data);
-        });
-
         postService.getPostTable(userId, page).then(response => {
             let data = response.data;
             setTotalElements(data.totalElements);
@@ -72,13 +41,6 @@ const UserHome = (props) => {
             let data = response.data || [];
             setMyCollectionList(data);
         });
-
-        //检查当前用户是否已经关注
-        if (sessionUser && sessionUser.userId != userId) {
-            userService.existsFollow({ fromId: sessionUser.userId, toId: userId }).then(resp => {
-                setFollowed(resp.data);
-            });
-        };
     }, [userId]);
 
     //瀑布流布局响应式断点 TODO:需要再优化一下
@@ -90,86 +52,9 @@ const UserHome = (props) => {
         500: 1
     };
 
-    /**
-     * 处理用户关注和取消关注操作
-     * @returns 
-     */
-    const handleFollow = async () => {
-        //如果没有登录，跳转到登录页面
-        if (!sessionUser) {
-            navigate("sign-in");
-            return;
-        }
-        if (followed) {
-            await userService.unfollow({ fromId: sessionUser.userId, toId: userId }).then(response => {
-                setFollowed(false);
-            });
-        } else {
-            await userService.follow({ fromId: sessionUser.userId, toId: userId }).then(response => {
-                setFollowed(true);
-            });
-        }
-    }
-
     return (
         <div className="user-home-container">
-            <div className="user-info">
-                <div className="user-avatar">
-                    <img src={userDetail.avatarURL || 'https://via.placeholder.com/150'} alt="User Avatar" />
-                </div>
-                <div className="user-details">
-                    <span>{userDetail.nickName}</span>
-                    <div className="user-stats">
-                        <p>
-                            <span>粉丝</span>
-                            <span>{followerCount}</span>
-                        </p>
-                        <p>
-                            <span>关注</span>
-                            <span>{followingCount}</span>
-                        </p>
-                        <p>
-                            <span>获赞</span>
-                            <span>{likedCount}</span>
-                        </p>
-                    </div>
-                    <div className="info2">
-                        <span>用户号: {userDetail.userId}</span>
-                        <span>{userDetail.city}</span>
-                        <span>{userDetail.education}</span>
-                    </div>
-                    <p>{userDetail.remark || "暂时没有介绍。"}</p>
-                    <div className='operations'>
-                        <div>
-                            {
-                                followed
-                                    ?
-                                    <>
-                                        <button className='btn btn-default' onClick={handleFollow}>已关注</button>
-                                    </>
-                                    :
-                                    <></>
-                            }
-                            {
-                                ((sessionUser?.userId != userId) && !followed)
-                                    ?
-                                    <>
-                                        <button className='btn btn-primary' onClick={handleFollow}>关注</button>
-                                    </>
-                                    :
-                                    <></>
-                            }
-                            {
-                                (sessionUser && sessionUser.userId == userId)
-                                    ?
-                                    <button className='btn btn-primary' onClick={() => { navigate(`/manage/user-profile/${sessionUser.userId}`); }}>编辑资料</button>
-                                    :
-                                    <></>
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <UserInfoHorizontal userId={userId}></UserInfoHorizontal>
             <div className="tab-content">
                 <TabView onBeforeTabChange={
                     e => { return e.index === 2 ? false : true; }
---
+++ src/app/blog/user/user-info-horizontal/index.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import userService from 'src/app/service/user-service';
+import './index.scss';
+
+const UserInfoHorizontal = (props) => {
+    //userId
+    const { userId } = props;
+
+    //用户详情
+    const [userDetail, setUserDetail] = useState({});
+
+    //粉丝数
+    const [followerCount, setFollowerCount] = useState(0);
+
+    //关注数
+    const [followingCount, setFollowingCount] = useState(0);
+
+    //获赞数
+    const [likedCount, setLikedCount] = useState(0);
+
+    //从 redux 中获取当前登录用户
+    const sessionUser = useSelector((state) => state.session.user) || {};
+
+    //导航对象
+    const navigate = useNavigate();
+
+    //是否已经关注
+    const [followed, setFollowed] = useState(false);
+
+    useEffect(() => {
+        userService.getUserFollowerCount(userId).then(response => {
+            setFollowerCount(response.data);
+        });
+        userService.getUserFollowingCount(userId).then(response => {
+            setFollowingCount(response.data);
+        });
+        userService.getUserLikedCount(userId).then(response => {
+            setLikedCount(response.data);
+        });
+        userService.getUserDetails(userId).then(response => {
+            setUserDetail(response.data.data);
+        });
+
+        //检查当前用户是否已经关注
+        if (sessionUser && sessionUser.userId != userId) {
+            userService.existsFollow({ fromId: sessionUser.userId, toId: userId }).then(resp => {
+                setFollowed(resp.data);
+            });
+        };
+    }, [userId]);
+
+    /**
+     * 处理用户关注和取消关注操作
+     * @returns 
+     */
+    const handleFollow = async () => {
+        //如果没有登录，跳转到登录页面
+        if (!sessionUser) {
+            navigate("sign-in");
+            return;
+        }
+        if (followed) {
+            await userService.unfollow({ fromId: sessionUser.userId, toId: userId }).then(response => {
+                setFollowed(false);
+            });
+        } else {
+            await userService.follow({ fromId: sessionUser.userId, toId: userId }).then(response => {
+                setFollowed(true);
+            });
+        }
+    }
+
+    return (
+        <div className="user-info">
+            <div className="user-avatar">
+                <img src={userDetail.avatarURL || 'https://via.placeholder.com/150'} alt="User Avatar" />
+            </div>
+            <div className="user-details">
+                <span>{userDetail.nickName}</span>
+                <div className="user-stats">
+                    <p>
+                        <span>粉丝</span>
+                        <span>{followerCount}</span>
+                    </p>
+                    <p>
+                        <span>关注</span>
+                        <span>{followingCount}</span>
+                    </p>
+                    <p>
+                        <span>获赞</span>
+                        <span>{likedCount}</span>
+                    </p>
+                </div>
+                <div className="info2">
+                    <span>用户号: {userDetail.userId}</span>
+                    <span>{userDetail.city}</span>
+                    <span>{userDetail.education}</span>
+                </div>
+                <p>{userDetail.remark || "暂时没有介绍。"}</p>
+                <div className='operations'>
+                    <div>
+                        {
+                            followed
+                                ?
+                                <>
+                                    <button className='btn btn-default' onClick={handleFollow}>已关注</button>
+                                </>
+                                :
+                                <></>
+                        }
+                        {
+                            ((sessionUser?.userId != userId) && !followed)
+                                ?
+                                <>
+                                    <button className='btn btn-primary' onClick={handleFollow}>关注</button>
+                                </>
+                                :
+                                <></>
+                        }
+                        {
+                            (sessionUser && sessionUser.userId == userId)
+                                ?
+                                <button className='btn btn-primary' onClick={() => { navigate(`/manage/user-profile/${sessionUser.userId}`); }}>编辑资料</button>
+                                :
+                                <></>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserInfoHorizontal;
