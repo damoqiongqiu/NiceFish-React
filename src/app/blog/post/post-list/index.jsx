26c70a11351c5ea84babc2d8e45cc62fa82a0b86
大漠穷秋 <damoqiongqiu@126.com>
2023-08-25 00:11:07 +0800

    post-list 实现滚动加载

--- src/app/blog/post/post-list/index.jsx
+++ src/app/blog/post/post-list/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Masonry from 'react-masonry-css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PostListItem from 'src/app/blog/post/post-list-item';
 import postService from 'src/app/service/post-service';
 import './index.scss';
@@ -8,33 +9,35 @@ export default props => {
   //文章列表
   const [postList, setPostList] = useState([]);
 
+  //滚动加载，是否还有更多数据
+  const [hasMore, setHasMore] = useState(true);
+
   //分页参数
   const [first, setFirst] = useState(0);
   const [rows, setRows] = useState(20);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [totalElements, setTotalElements] = useState(0);
-
-  /**
-   * 分页事件
-   * @param {*} event 
-   */
-  const onPageChange = (event) => {
-    setFirst(event.first);
-    setRows(event.rows);
-    setPage(event.page + 1);
-  };
 
   /**
    * 加载文章列表
    */
-  useEffect(() => {
-    postService.getPostList(page, rows).then(response => {
+  const fetchMoreData = async () => {
+    let temp = page + 1;
+    setPage(temp);
+
+    await postService.getPostList(temp, rows).then(response => {
       let data = response.data;
       setTotalElements(data.totalElements);
 
+      setHasMore(!data.last);
+
       data = data?.content || [];
-      setPostList(data);
+      setPostList([...postList, ...data]);
     });
+  }
+
+  useEffect(() => {
+    fetchMoreData();
   }, []);
 
   //瀑布流布局响应式断点 TODO:需要再优化一下
@@ -48,18 +51,26 @@ export default props => {
 
   return (
     <div className='post-list-container'>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column">
-        {
-          postList.map((item, index) => {
-            return (
-              <PostListItem postDetail={item} key={index}></PostListItem>
-            );
-          })
-        }
-      </Masonry>
-    </div>
+      <InfiniteScroll
+        dataLength={postList.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        style={{ overflow: "inherit" }}
+      >
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column">
+          {
+            postList.map((item, index) => {
+              return (
+                <PostListItem postDetail={item} key={index}></PostListItem>
+              );
+            })
+          }
+        </Masonry>
+      </InfiniteScroll>
+    </div >
   );
 };
